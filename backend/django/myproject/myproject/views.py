1bd5df4293918102ad1d17a9661b33f2b1ea9fe6
Carlos <carloszaragozabeato15@gmail.com>
2024-06-24 16:22:21 +0200

    s

--- backend/django/myproject/myproject/views.py
+++ backend/django/myproject/myproject/views.py
@@ -1,10 +1,12 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def homepage(request):
-    # return HttpResponse("HELLO WORLD!!")
+    # return HttpResponse("Hello World! I'm Home.")
     return render(request, 'home.html')
 
+
 def about(request):
-    # return HttpResponse("ABOUT PAGE")
+    # return HttpResponse("My About page.")
     return render(request, 'about.html')
