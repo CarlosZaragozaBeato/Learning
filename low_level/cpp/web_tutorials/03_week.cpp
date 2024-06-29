b83d94aa6324f7505891804897b4e96ef63d0ec6
Carlos <carloszaragozabeato15@gmail.com>
2024-06-29 14:49:05 +0200

    s

---
+++ low_level/cpp/web_tutorials/03_week.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // ### Semana 3: Programación Orientada a Objetos (OOP)
+    // - **Día 15**: Principios de OOP.
+    // - **Día 16**: Herencia.
+    // - **Día 17**: Polimorfismo.
+    // - **Día 18**: Sobrecarga de operadores.
+    // - **Día 19**: Plantillas (templates).
+    // - **Día 20**: Excepciones.
+    // - **Día 21**: Repaso y ejercicios prácticos.
+
+    return 0;
+}
